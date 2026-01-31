4ab241f43273f05d98d44d07074b0a4ae89b7509
dstuck <david.e.stuck@gmail.com>
2026-01-31 12:53:59 -0600

    Pulse to the music beat

---
+++ Assets/Scripts/Music/BeatManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public sealed class BeatManager : MonoBehaviour
+{
+    [SerializeField] private float _bpm = 75f;
+    [SerializeField] private AudioSource _audioSource;
+    [Header("Events")]
+    [SerializeField] private Intervals[] _intervals;
+
+    private void Update()
+    {
+        if (_audioSource == null || _audioSource.clip == null)
+        {
+            return;
+        }
+
+        foreach (Intervals interval in _intervals)
+        {
+            float sampledTime =
+                (_audioSource.timeSamples / (_audioSource.clip.frequency * interval.GetIntervalLength(_bpm)));
+            interval.CheckForNewInterval(sampledTime);
+        }
+    }
+}
+
+[System.Serializable]
+public sealed class Intervals
+{
+    [SerializeField] private float _steps = 1f;
+    [SerializeField] private UnityEvent _trigger;
+    private int _lastInterval;
+
+    public float GetIntervalLength(float bpm)
+    {
+        return 60f / (bpm * _steps);
+    }
+
+    public void CheckForNewInterval(float interval)
+    {
+        if (Mathf.FloorToInt(interval) != _lastInterval)
+        {
+            _lastInterval = Mathf.FloorToInt(interval);
+            _trigger?.Invoke();
+        }
+    }
+}
